fcfa373f361f8877593bf639f05db82998952348
NaijasonEzeiru <67031753+NaijasonEzeiru@users.noreply.github.com>
2023-07-13 13:20:52 +0100

    fixed __dirname not defined error

--- dist/utils/multer.js
+++ dist/utils/multer.js
@@ -1,5 +1,9 @@
 import { diskStorage } from 'multer';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+var __filename = fileURLToPath(import.meta.url);
+var __dirname = dirname(__filename);
 var storage = diskStorage({
     destination: function (req, file, callback) {
         callback(null, path.join(__dirname, '/uploads/'));
--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -1,5 +1,10 @@
 import { diskStorage } from 'multer';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const storage = diskStorage({
 	destination(req, file, callback) {
